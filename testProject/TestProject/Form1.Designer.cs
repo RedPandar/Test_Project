e28248b8de0bd9747d9d6dcc0d3040de8dd54d27
RedPandar <izambezik@gmail.com>
2018-03-18 22:50:51 +0500

    Add files via upload

---
+++ testProject/TestProject/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load_file_btn = new System.Windows.Forms.Button();
+            this.dGV = new System.Windows.Forms.DataGridView();
+            this.Save_to_file_btn = new System.Windows.Forms.Button();
+            this.wrd_count_label = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.wrd_count_label_text = new System.Windows.Forms.Label();
+            this.line_count_label_text = new System.Windows.Forms.Label();
+            this.line_count_label = new System.Windows.Forms.Label();
+            this.Sort_check = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // load_file_btn
+            // 
+            this.load_file_btn.Location = new System.Drawing.Point(12, 12);
+            this.load_file_btn.Name = "load_file_btn";
+            this.load_file_btn.Size = new System.Drawing.Size(144, 31);
+            this.load_file_btn.TabIndex = 0;
+            this.load_file_btn.Text = "Load";
+            this.load_file_btn.UseVisualStyleBackColor = true;
+            this.load_file_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dGV
+            // 
+            this.dGV.AllowUserToAddRows = false;
+            this.dGV.AllowUserToDeleteRows = false;
+            this.dGV.AllowUserToResizeColumns = false;
+            this.dGV.AllowUserToResizeRows = false;
+            this.dGV.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dGV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dGV.Location = new System.Drawing.Point(174, 12);
+            this.dGV.MultiSelect = false;
+            this.dGV.Name = "dGV";
+            this.dGV.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dGV.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dGV.Size = new System.Drawing.Size(267, 449);
+            this.dGV.TabIndex = 1;
+            // 
+            // Save_to_file_btn
+            // 
+            this.Save_to_file_btn.Location = new System.Drawing.Point(12, 49);
+            this.Save_to_file_btn.Name = "Save_to_file_btn";
+            this.Save_to_file_btn.Size = new System.Drawing.Size(144, 31);
+            this.Save_to_file_btn.TabIndex = 2;
+            this.Save_to_file_btn.Text = "To file";
+            this.Save_to_file_btn.UseVisualStyleBackColor = true;
+            this.Save_to_file_btn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // wrd_count_label
+            // 
+            this.wrd_count_label.AutoSize = true;
+            this.wrd_count_label.Location = new System.Drawing.Point(117, 362);
+            this.wrd_count_label.Name = "wrd_count_label";
+            this.wrd_count_label.Size = new System.Drawing.Size(40, 13);
+            this.wrd_count_label.TabIndex = 3;
+            this.wrd_count_label.Text = "№№№";
+            // 
+            // wrd_count_label_text
+            // 
+            this.wrd_count_label_text.AutoSize = true;
+            this.wrd_count_label_text.Location = new System.Drawing.Point(9, 361);
+            this.wrd_count_label_text.Name = "wrd_count_label_text";
+            this.wrd_count_label_text.Size = new System.Drawing.Size(96, 13);
+            this.wrd_count_label_text.TabIndex = 4;
+            this.wrd_count_label_text.Text = "Количество слов:";
+            // 
+            // line_count_label_text
+            // 
+            this.line_count_label_text.AutoSize = true;
+            this.line_count_label_text.Location = new System.Drawing.Point(9, 386);
+            this.line_count_label_text.Name = "line_count_label_text";
+            this.line_count_label_text.Size = new System.Drawing.Size(101, 13);
+            this.line_count_label_text.TabIndex = 6;
+            this.line_count_label_text.Text = "Количество строк:";
+            // 
+            // line_count_label
+            // 
+            this.line_count_label.AutoSize = true;
+            this.line_count_label.Location = new System.Drawing.Point(117, 387);
+            this.line_count_label.Name = "line_count_label";
+            this.line_count_label.Size = new System.Drawing.Size(40, 13);
+            this.line_count_label.TabIndex = 5;
+            this.line_count_label.Text = "№№№";
+            // 
+            // Sort_check
+            // 
+            this.Sort_check.AutoSize = true;
+            this.Sort_check.Checked = true;
+            this.Sort_check.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Sort_check.Location = new System.Drawing.Point(35, 86);
+            this.Sort_check.Name = "Sort_check";
+            this.Sort_check.Size = new System.Drawing.Size(91, 17);
+            this.Sort_check.TabIndex = 7;
+            this.Sort_check.Text = "Сортировать";
+            this.Sort_check.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "wrd_count_file";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 473);
+            this.Controls.Add(this.Sort_check);
+            this.Controls.Add(this.line_count_label_text);
+            this.Controls.Add(this.line_count_label);
+            this.Controls.Add(this.wrd_count_label_text);
+            this.Controls.Add(this.wrd_count_label);
+            this.Controls.Add(this.Save_to_file_btn);
+            this.Controls.Add(this.dGV);
+            this.Controls.Add(this.load_file_btn);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load_file_btn;
+        private System.Windows.Forms.DataGridView dGV;
+        private System.Windows.Forms.Button Save_to_file_btn;
+        private System.Windows.Forms.Label wrd_count_label;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label wrd_count_label_text;
+        private System.Windows.Forms.Label line_count_label_text;
+        private System.Windows.Forms.Label line_count_label;
+        private System.Windows.Forms.CheckBox Sort_check;
+    }
+}
+
---
+++ testProject/TestProject/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Чистка датагрида
+            dGV.Rows.Clear();
+            //Вызов функций
+            File_load();
+            wrd_count();
+        }
+
+        //Объявление глобального списка
+        List<string> wrd_list = new List<string>();
+        //Функция чтения файла по строчно + составения списка всех слов (С повторениями)
+        public void File_load()
+        {
+            //Обьявление переменных для подсчета
+            int wrd_counter = 0;
+            int str_counter = 0;
+            string line;
+            //Разделители строк на слова
+            string[] sp = new string[] { ",", ".", "!", "\'", " ", "\'s", ":", "(","-",")" ,"|" ,"/"};
+            //Фильтр для формы открытия файла 
+            openFileDialog1.Filter = "Text Files|*.txt";
+            openFileDialog1.Title = "Select a txt File";
+            //Считывание файла
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //Считывание текстового файла
+                System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog1.FileName);
+                //По строчная обработка
+                while ((line = file.ReadLine()) != null)
+                {
+                    str_counter++;
+                    //Разделение строк на слова
+                    foreach (string words in line.Split(sp, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        //Проверка слов на наличие символов не удовлетворящих условиям
+                        if (words.All(char.IsLetterOrDigit))
+                        {
+                            //Добавление слов в список
+                            wrd_list.Add(words);
+                            wrd_counter++;
+                        }
+                    }
+                }
+            //Вывод на форму
+            wrd_count_label.Text = Convert.ToString(wrd_counter);
+            line_count_label.Text = Convert.ToString(str_counter);
+            file.Close();
+            }
+        }
+        Dictionary<string, int> counts = new Dictionary<string, int>();
+        //Функция подсчета слов в списке составленном в функции file_load
+        public void wrd_count()
+        {
+            //Для каждого значения в списке слов выполняется подсчет в тексте
+            foreach (string value in wrd_list)
+            {
+                if (counts.ContainsKey(value))
+                    counts[value] = counts[value] + 1;
+                else
+                    counts.Add(value, 1);
+            }
+            //Вывод в датагрид 
+            BindingSource bgS = new BindingSource();
+            dGV.DataSource = bgS;
+            bgS.DataSource = counts;
+        }
+        //Запись в файл
+        public void wrds_to_file()
+        {
+           // string path = @"C:\Test.txt";
+            System.IO.File.Delete(@"C:\test.txt");
+            //Запрос на сортировку
+            if (Sort_check.Checked == true)
+            {
+                //Сортировка с помощью linq
+                var sortedCounts = from entry in counts orderby entry.Value descending select entry;
+                //Запись в файл с сортировкой
+                foreach (KeyValuePair<string, int> kvp in sortedCounts)
+                {
+                    System.IO.File.AppendAllText("Test.txt", string.Format("{0} {1} {2}", kvp.Key, kvp.Value, Environment.NewLine));
+                }
+            }
+            else
+                //Запись в файл без сортировки
+                foreach (KeyValuePair<string, int> kvp in counts)
+                {
+                    System.IO.File.AppendAllText("Test.txt", string.Format("{0} {1} {2}", kvp.Key, kvp.Value, Environment.NewLine));
+                }
+
+        //Открытие файла после сохранения
+        //Настроки окна сообщения
+        DialogResult result = MessageBox.Show(
+        "Файл сохранен. Открыть?",
+        "Сообщение",
+        MessageBoxButtons.YesNo,
+        MessageBoxIcon.Information,
+        MessageBoxDefaultButton.Button1,
+        MessageBoxOptions.DefaultDesktopOnly);
+
+        //Если "Да", то открыть файл
+        if (result == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("Test.txt");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wrds_to_file();
+        }
+    }
+}
